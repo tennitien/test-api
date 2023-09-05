97fd6ff8c4538d60203008362b9c286a587b5305
tennitien <ntctien.forwork@gmail.com>
2023-09-05 20:53:49 +0700

    server

--- server/index.js
+++ server/index.js
@@ -1,39 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const RegisterModel = require('./models/Register');
 
-const app = express()
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json())
+const app = express();
+app.use(
+  cors({
+    origin: ['https://deploy-mern-frontend.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(
+  'mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority'
+);
 
-
-app.get("/", (req, res) => {
-    res.json("Hello");
-})
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-})
-
+app.get('/', (req, res) => {
+  res.json('Hello');
+});
+app.get('/api', (req, res, next) => {
+  res.json('Api');
+});
 
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+  console.log('Server is Running');
+});
