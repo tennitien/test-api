4838181fea47f69cf0925b239628cb6f1638f7b1
tennitien <ntctien.forwork@gmail.com>
2023-09-05 20:55:03 +0700

    server

--- server/index.js
+++ server/index.js
@@ -17,12 +17,12 @@ mongoose.connect(
   'mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority'
 );
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json('Hello');
 });
-app.get('/api', (req, res, next) => {
-  res.json('Api');
-});
+// app.get('/api', (req, res, next) => {
+//   res.json('Api');
+// });
 
 app.listen(3001, () => {
   console.log('Server is Running');
